6168bd6c2f48b2fd244fe65b1f9462c707fafa18
printf83 <printf83@gmail.com>
2021-10-23 05:14:43 +0800

    allow attr change from etented class

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 $(document).ready(() => {
 	var root = document.getElementById("root");
 	var b = new tag({
+		tag: "div",
 		elem: [
 			"Hello World",
 			new button({ label: "Primary", color: "primary" }),
-			new button({ label: "Danger", color: "danger", type: "submit", icon: { icon: "fire" } }),
+			new button({
+				label: "Danger",
+				color: "danger",
+				type: "submit",
+				icon: { icon: "facebook", style: "fab" },
+			}),
+			new button("hello", "warning"),
 		],
 		attr: { class: "display-1" },
 	});
--- js/base/build.js
+++ js/base/build.js
@@ -43,6 +43,110 @@ class attr {
 		this.d = d;
 	}
 
+	static #r = {
+		class: function (a, b) {
+			let aT = typeof a;
+			let bT = typeof a;
+			let aR = Array.isArray(a);
+			let bR = Array.isArray(b);
+
+			if (!aR && !bR && aT === "string" && bT === "string") {
+				return [a, b];
+			} else if (!aR && bR && aT === "string") {
+				b.push(a);
+				return b;
+			} else if (aR && !bR && bT === "string") {
+				a.push(b);
+				return a;
+			} else if (aR && bR) {
+				return [...a, ...b];
+			} else {
+				console.error("Unhandle #r class rules");
+			}
+		},
+		style: function (a, b) {
+			let c = {};
+			Object.keys(a).forEach((i) => {
+				if (b.hasOwnProperty(i)) {
+					console.warn(`Same property ${i} provided in 'a' and 'b'. Using style from 'a' insted.`);
+					c[i] = a[i]; //used a insted
+				} else {
+					c[i] = a[i];
+				}
+			});
+
+			Object.keys(b).forEach((i) => {
+				if (!a.hasOwnProperty(i)) {
+					c[i] = b[i];
+				}
+			});
+
+			return c;
+		},
+		data: function (a, b) {
+			let c = {};
+			Object.keys(a).forEach((i) => {
+				if (b.hasOwnProperty(i)) {
+					console.warn(`Same property ${i} provided in 'a' and 'b'. Using data from 'a' insted.`);
+					c[i] = a[i]; //used a insted
+				} else {
+					c[i] = a[i];
+				}
+			});
+
+			Object.keys(b).forEach((i) => {
+				if (!a.hasOwnProperty(i)) {
+					c[i] = b[i];
+				}
+			});
+
+			return c;
+		},
+	};
+
+	//rules for unsupported property
+	static merge = function (a, b, rules) {
+		if ((a || b) && !(a && b)) {
+			return a || b;
+		} else if (a && b) {
+			//manual copy needed
+			let c = {};
+			Object.keys(a).forEach((i) => {
+				if (b.hasOwnProperty(i)) {
+					//need to merge a and b into c
+					switch (i) {
+						case "class":
+						case "style":
+						case "data":
+							c[i] = this.#r[i](a[i], b[i]);
+							break;
+						default:
+							if (rules && rules.hasOwnProperty(i)) {
+								c[i] = rules[i](a[i], b[i]);
+							} else {
+								console.warn(
+									`Fail to merge attr:${i}. No rules provided for merging this attribute. Using attr from 'a' insted.`
+								);
+								c[i] = a[i]; //used a insted
+							}
+					}
+				} else {
+					c[i] = a[i];
+				}
+			});
+
+			Object.keys(b).forEach((i) => {
+				if (!a.hasOwnProperty(i)) {
+					c[i] = b[i];
+				}
+			});
+
+			return c;
+		} else {
+			return null;
+		}
+	};
+
 	attach = function (elems) {
 		if (elems) {
 			Object.keys(this.d).forEach((i) => {
@@ -90,7 +194,7 @@ class tag {
 		d = core.extend(
 			{},
 			{
-				tag: "div",
+				tag: null,
 				attr: null,
 				elem: null,
 			},
@@ -103,7 +207,7 @@ class tag {
 		let hasContainer = container ? true : false;
 		container = container || document.createElement("div");
 
-		let element = document.createElement(this.d.tag);
+		let element = this.d.tag ? document.createElement(this.d.tag) : container;
 		if (this.d.attr) {
 			element = new attr(this.d.attr).attach(element);
 		}
@@ -126,7 +230,7 @@ class tag {
 			}
 		}
 
-		container.appendChild(element);
+		if (this.d.tag) container.appendChild(element);
 
 		if (hasContainer) {
 			return container;
@@ -142,11 +246,37 @@ class tag {
 	};
 }
 
+class label extends tag {
+	constructor(d) {
+		d = core.extend(
+			{},
+			{
+				attr: null,
+
+				icon: null,
+				label: null,
+			},
+			d
+		);
+
+		let e = {
+			attr: d.attr,
+			elem: d.icon
+				? [new icon(d.icon), new tag({ tag: "label", attr: { class: "ms-2" }, elem: d.label })]
+				: new tag({ tag: "label", elem: d.label }),
+		};
+
+		super(e);
+	}
+}
+
 class button extends tag {
 	constructor(d) {
 		d = core.extend(
 			{},
 			{
+				attr: null,
+
 				type: "button",
 				label: "Button",
 				icon: null,
@@ -160,7 +290,7 @@ class button extends tag {
 
 		let e = {
 			tag: "button",
-			attr: {
+			attr: attr.merge(d.attr, {
 				role: "button",
 				type: d.type,
 				onclick: d.onclick,
@@ -169,10 +299,8 @@ class button extends tag {
 					d.color ? (d.outline ? `btn-outline-${d.color}` : `btn-${d.color}`) : null,
 					d.textcolor ? `text-${d.textcolor}` : null,
 				],
-			},
-			elem: d.icon
-				? [new icon(d.icon), new tag({ tag: "label", attr: { class: "ms-2" }, elem: d.label })]
-				: d.label,
+			}),
+			elem: new label({ icon: d.icon, label: d.label }),
 		};
 
 		super(e);
@@ -184,6 +312,8 @@ class icon extends tag {
 		d = core.extend(
 			{},
 			{
+				attr: null,
+
 				style: "fas",
 				icon: null,
 			},
@@ -192,9 +322,9 @@ class icon extends tag {
 
 		let e = {
 			tag: "i",
-			attr: {
+			attr: attr.merge(d.attr, {
 				class: [d.style, `fa-${d.icon}`],
-			},
+			}),
 		};
 
 		super(e);
