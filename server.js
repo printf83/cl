b607c5cf158070b28889f0df5d36a11a12ade9d9
printf83 <printf83@gmail.com>
2022-01-09 18:06:31 +0800

    using helmet

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 "use strict";
 var path = require("path");
 var express = require("express");
+var helmet = require("helmet");
 const app = express();
 
 app.set("trust proxy", 1);
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "docs"), { maxAge: 3600000 }));
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "docs")));
 
 const envPORT = process.env.PORT || 8081;
