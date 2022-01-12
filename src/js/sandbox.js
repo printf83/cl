a498d54a3c48fe75ad07ac2f087266bf2c5c6014
printf83 <printf83@gmail.com>
2022-01-12 10:46:14 +0800

    add sandbox query data

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -9,11 +9,46 @@ $.core.documentReady(() => {
 			color: "primary",
 			icon: "fire",
 			onclick: function () {
-				new $.query({}, [
-					function (event, data) {
-						new $.toast("/", JSON.stringify(data)).show();
+				new $.query(
+					{
+						field: [
+							{ value: "name", label: "Name", type: "text" },
+							{ value: "dob", label: "Date Of Birth", type: "date" },
+							{ value: "phone", label: "Phone", type: "tel" },
+							{ value: "picture", label: "Picture", type: "check" },
+							{ value: "email", label: "Email", type: "email" },
+							{
+								value: "state",
+								label: "State",
+								type: "select",
+								option: null,
+								placeholder: "Please Choose One",
+							},
+						],
+						data: {
+							filter: null,
+							sort: { state: -1, name: 1 },
+							field: { __v: 0 },
+							limit: 10,
+							skip: 0,
+						},
+						limit: {
+							min: 1,
+							max: 100,
+							step: 5,
+						},
+						skip: {
+							min: 1,
+							max: 100,
+							step: 1,
+						},
 					},
-				]).show();
+					[
+						function (event, data) {
+							new $.toast("/", JSON.stringify(data)).show();
+						},
+					]
+				).show();
 			},
 		})
 	);
