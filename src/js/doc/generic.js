a6ad93a906d89f03e91e42a31421dd38e548c0c0
printf83 <printf83@gmail.com>
2022-02-14 12:21:36 +0800

    generic db doc

--- src/js/doc/generic.js
+++ src/js/doc/generic.js
@@ -43,7 +43,19 @@ export default [
 							class: "prettyprint lang-js",
 							marginbottom: 0,
 							elem: beautifyjs(`
-								require("./routes/generic.js")(app, "customer");
+								const mongoose = require("mongoose");
+
+								const schema = mongoose.Schema({
+									name: { type: String, require: true },
+									dob: { type: String, require: true },
+									phone: { type: String, require: true },
+									email: String,
+									picture: String,
+									state: String,
+								});
+								const db = mongoose.model("customer", schema);
+								module.exports = { db, schema };
+
 							`),
 						})
 					)
