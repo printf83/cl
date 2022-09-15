332026cdb3db608d6fc8838b1f250f3715ec0a95
printf83 <printf83@gmail.com>
2022-09-15 13:23:59 +0800

    update

--- src/js/doc/toast.js
+++ src/js/doc/toast.js
@@ -169,7 +169,6 @@ export default [
 	{
 		title: "Color",
 		container: sample.vstackcontainer,
-
 		import: ["toast", "msg", "sample"],
 		code: () => {
 			return ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"].map((i) => {
