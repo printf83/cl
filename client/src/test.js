d9bb99202e19e543a92743ce53ab085780260ea8
printf83 <printf83@gmail.com>
2022-11-20 21:30:05 +0800

    btnclose using fix

--- client/src/test.js
+++ client/src/test.js
@@ -1,24 +1,19 @@
 "use strict";
 
-///library
 import * as core from "./dist/cl/base/core.js";
-import tag from "./dist/cl/base/tag.js";
+import button from "./dist/cl/base/button.js";
+import p from "./dist/cl/base/p.js";
 
-///code
 let code = () => {
 	return [
-		new tag({
-			tag: "div",
-			elem: "Example with style",
-
-			border: "2px solid #aaa",
-			backgroundColor: "rgba(255,0,0,0.5)",
-			padding: "1rem",
+		new button({
+			label: "Hello World!",
+			color: "primary",
 		}),
+		new p("Hello World!"),
 	];
 };
 
-///loader
 core.documentReady(() => {
 	core.replaceChild(document.getElementById("root"), code());
 });
