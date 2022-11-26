d3d510c764b2440875e6e1095ac4e3624309f93b
printf83 <printf83@gmail.com>
2022-11-27 00:32:05 +0800

    remove last update

--- source/cl/base/navbar.js
+++ source/cl/base/navbar.js
@@ -150,7 +150,7 @@ export class toggle extends collapse.toggle {
 					// label: "Toggle navigation",
 					// hidelabel: true,
 					class: "navbar-toggler",
-					display: "inline-block",
+					// display: "inline-block",
 					elem: new span({ class: "navbar-toggler-icon" }),
 				}),
 			});
